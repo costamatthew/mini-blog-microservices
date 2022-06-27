913ae992da9067166f796cc9e8a16f2610228ea0
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-06-26 22:49:11 -0300

    fix: style code and add catch

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,20 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
-app.post("/event", (req, res) => {
-  const event = req.body;
+app.post('/event', (req, res) => {
+  const event = req.body
 
-  axios.post("http://localhost:4000/event", event);
-  axios.post("http://localhost:4001/event", event);
-  axios.post("http://localhost:4002/event", event);
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message)
+  })
 
-  res.send({ status: "OK" });
-});
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message)
+  })
+
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message)
+  })
+
+  res.send({ status: 'OK' })
+})
 
 app.listen(4005, () => {
-  console.log("Listening on http://localhost:4005");
-});
+  console.log('Listening on http://localhost:4005')
+})
